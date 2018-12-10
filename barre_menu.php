ab900626899bfb4cfe9ef141782b9551e1fc5383
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-10 09:53:02 +0100

    Index + Menu

---
+++ barre_menu.php
@@ -0,0 +1,31 @@
+<!--header-------------->
+<div id="header">
+  <a href="C:\laragon\www\UMAKE\espace_membre\espacemembre.html"> <img src="Images\espacemembre.png" alt="Mon Espace" height="30px" width="auto" style=" margin-left: 90%"> Mon espace</a>
+</div>
+<!--menu-------------->
+<div id="menu">
+   <ul>
+   <li>
+   <a href="index.php">ACCUEIL</a>
+    </li>
+    <li><a href="Cours\Digital\presentation_dev.html"> DEV ▼ </a>
+    <ul>
+      <li><a href="#">Langages web</a></li>
+      <li><a href="#">Langages logiciel</a></li>
+      <li><a href="#">Langages mobile</a></li>
+
+    </ul>
+    <li><a href="Cours\Digital\presentation_digi.html">DIGI ▼ </a>
+    <ul>
+     <li><a href="...................">Listes des cours </a></li>
+     <li><a href="...................">Derniers cours</a></li>
+     <li><a href="...................">Cours populaire</a></li>
+    </ul>
+    <li><a href="Cours\Digital\presentation_ops.html">OPS ▼</a>
+    <ul>
+     <li><a href="...................">Listes des cours</a></li>
+      <li><a href="...................">Derniers cours</a></li>
+      <li><a href="...................">Cours populaire</a></li>
+   </ul>
+   </li>
+   </ul>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Blog IMIE</title>
+    <link rel="stylesheet" href="style.css">
+    
+
+    <script src="script.js"></script>
+  </head>
+
+  <body>
+
+  <?php include("barre_menu.php"); ?>
+
+  </body>
+
+</html>
