3f82ef8e17f9c8f32a1cadf2b70357924a3bb775
ValentinCloarec <valentin.cloarec@imie.fr>
2018-12-10 11:39:17 +0100

    page dev

---
+++ presentation_dev.php
@@ -0,0 +1,31 @@
+<!--header-------------->
+<div id="header">
+  <a href="espacemembre.html"> <img src="images\espacemembre.png" alt="Mon Espace" height="30px" width="auto" style=" margin-left: 90%"></a>
+</div>
+<!--menu-------------->
+<div id="menu">
+   <ul>
+   <li>
+   <a href="index.php">ACCUEIL</a>
+    </li>
+    <li><a href="Cours\Digital\presentation_dev.html"> DEV ▼ </a>
+    <ul>
+      <li><a href="#">Langages web</a></li>
+      <li><a href="#">Langages logiciel</a></li>
+      <li><a href="#">Langages mobile</a></li>
+
+    </ul>
+    <li><a href="Cours\Digital\presentation_digi.html">DIGI ▼ </a>
+    <ul>
+     <li><a href="...................">Listes des cours </a></li>
+     <li><a href="...................">Derniers cours</a></li>
+     <li><a href="...................">Cours populaire</a></li>
+    </ul>
+    <li><a href="Cours\Digital\presentation_ops.html">OPS ▼</a>
+    <ul>
+     <li><a href="...................">Listes des cours</a></li>
+      <li><a href="...................">Derniers cours</a></li>
+      <li><a href="...................">Cours populaire</a></li>
+   </ul>
+   </li>
+   </ul>
