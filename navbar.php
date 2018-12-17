cf8ee40207e17e9014a1c83adff63ae6215c44fe
aloonska <maroussia.gossmann@yahoo.fr>
2018-12-17 16:41:01 +0100

    ajout espace membre php et modif du navbar

--- navbar.php
+++ navbar.php
@@ -1,13 +1,13 @@
 <!--header-------------->
   <div id="header">
 
-      <a href="espacemembre.php"> <img class="logo_user" src="images\espacemembre.png" alt="Mon Espace"  height="30px"></a>
+      <a href="http://localhost:30662"> <img class="logo_user" src="images\espacemembre.png" alt="Mon Espace"  height="30px"></a>
 
   </div>
 <!--menu-------------->
 <div id="navbar">
 
-  <a href="index.php"> <img class="logo_index" src="images\ecole_de_la_filiere_numerique.png" alt="Mon Espace"  height="60px"></a>
+  <a href="index.php"> <img class="logo_index" src="images\ecole_de_la_filiere_numerique.png" alt="Mon Espace"  height="40px"></a>
 
     <ul>
 
